8fe53055d024981bb56f5b6637adc7d61d860c80
Nenad Ticaric <nticaric@gmail.com>
2023-01-19 15:14:21 +0100

    registering routes

--- src/SalesManagementServiceProvider.php
+++ src/SalesManagementServiceProvider.php
@@ -8,9 +8,25 @@ class SalesManagementServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->registerRoutes();
     }
 
     public function register()
     {
+    }
+
+    protected function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        });
+    }
+
+    protected function routeConfiguration()
+    {
+        return [
+            'prefix' => config('sales-management.prefix'),
+            'middleware' => config('sales-management.middleware'),
+        ];
     }
 }
---
+++ src/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route; 
+
+Route::get('/sales', function() {
+    dd("hello world from sales-management");
+});
