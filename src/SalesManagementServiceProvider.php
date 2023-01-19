9be94608bd8c593cfafe7ad5acd1db3433d4ea0c
nticaric <nticaric@users.noreply.github.com>
2023-01-19 14:16:00 +0000

    Fix styling

--- src/SalesManagementServiceProvider.php
+++ src/SalesManagementServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Teamtnt\SalesManagement;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
 
 class SalesManagementServiceProvider extends ServiceProvider
 {
