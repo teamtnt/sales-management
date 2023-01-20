713975d1cc57b13775a44d68007e84eefc9dff09
Nenad Ticaric <nticaric@gmail.com>
2023-01-20 14:41:43 +0100

    navigation

--- src/resources/views/layouts/partials/left-navigation.blade.php
+++ src/resources/views/layouts/partials/left-navigation.blade.php
@@ -16,10 +16,37 @@
                 </a>
             </li>
 
-
             <li class="sidebar-item {{ (Route::currentRouteName() == "contacts.index") ? 'active' : '' }}">
                 <a class="sidebar-link" href="{{ route('contacts.index') }}">
                     <i class="align-middle" data-feather="user"></i> <span class="align-middle">{{ __("Contacts")}}</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="">
+                    <i class="align-middle" data-feather="home"></i> <span class="align-middle">{{ __("Companies")}}</span>
+                </a>
+            </li>
+            
+            <li class="sidebar-item ">
+                <a class="sidebar-link" href="">
+                    <i class="align-middle" data-feather="thumbs-up"></i> <span class="align-middle">{{ __("Task List")}}</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item {{ (Route::currentRouteName() == "pipelines.index") ? 'active' : '' }}">
+                <a class="sidebar-link" href="{{ route('pipelines.index') }}">
+                    <i class="align-middle" data-feather="list"></i> <span class="align-middle">{{ __("Pipelines")}}</span>
+                </a>
+            </li>
+            <li class="sidebar-item ">
+                <a class="sidebar-link" href="">
+                    <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">{{ __("Deals")}}</span>
+                </a>
+            </li>
+            <li class="sidebar-item ">
+                <a class="sidebar-link" href="">
+                    <i class="align-middle" data-feather="play-circle"></i> <span class="align-middle">{{ __("Automation")}}</span>
                 </a>
             </li>
         </ul>
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,5 @@ use Teamtnt\SalesManagement\Http\Controllers\ContactsController;
 
 Route::get('/dashboard', [ContactsController::class, 'index'])->name('dashboard');
 Route::get('/sales', [ContactsController::class, 'index'])->name('contacts.index');
+
+Route::get('/pipelines', [ContactsController::class, 'index'])->name('pipelines.index');
