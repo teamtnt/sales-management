47ff39c22c0ea529c20cbdb6c1f50baeb28b0bf8
Tome Perica <tome.perica@yahoo.com>
2023-01-26 12:16:06 +0100

    add actions

--- src/resources/views/contacts/actions.blade.php
+++ src/resources/views/contacts/actions.blade.php
@@ -1,6 +1,6 @@
 <div class="d-flex align-items-center">
-    <a href="" class="btn btn-sm btn-warning d-inline me-1">Edit</a>
-    <form action="" method="POST">
+    <a href="{{ route('contacts.edit', $id) }}" class="btn btn-sm btn-warning d-inline me-1">Edit</a>
+    <form action="{{ route('contacts.destroy', $id) }}" method="POST">
         {{ method_field('DELETE') }}
         {{ csrf_field() }}
         <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to remove this item?')"> Delete</button>
