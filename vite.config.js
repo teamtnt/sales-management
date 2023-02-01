ceed4a419f1248077c137f5dc5b48c7ce6739304
Tome Perica <tome.perica@yahoo.com>
2023-02-01 11:38:31 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,25 @@
-import {defineConfig, splitVendorChunkPlugin} from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
-export default defineConfig({
+// Vite configuration object
+let config = {
     build: {
         outDir: "public/sales-management",
-    },
-    plugins: [
-        splitVendorChunkPlugin(),
-        laravel({
-            publicDirectory: "public",
-            buildDirectory: "sales-management", // ako ovog nema, onda na npm run build woff2 fajlovi od fontawsomea gledaju u krivi dir
-            hotFile: "public/sales-management/hot",
-            input: ['src/resources/scss/light.scss', 'src/resources/js/app.js',],
-            refresh: true
-        }), vue({
-            template: {
-                transformAssetUrls: {
-                    base: null, includeAbsolute: false,
-                },
+    }, plugins: [splitVendorChunkPlugin(), laravel({
+        publicDirectory: "public",
+        buildDirectory: "sales-management", // ako ovog nema, onda na npm run build woff2 fajlovi od fontawsomea gledaju u krivi dir
+        hotFile: "public/sales-management/hot",
+        input: ['src/resources/scss/light.scss', 'src/resources/js/app.js',],
+        refresh: true
+    }), vue({
+        template: {
+            transformAssetUrls: {
+                base: null, includeAbsolute: false,
             },
-        }), VueI18nPlugin({ /* options */}),], resolve: {
+        },
+    }), VueI18nPlugin({ /* options */}),], resolve: {
         alias: {
             // '@': '/src/resources/js'
             '~bootstrap': 'bootstrap',
@@ -31,4 +29,20 @@ export default defineConfig({
             '~dragula': 'dragula'
         }
     }
-});
+}
+export default defineConfig(({command}) => {
+    if (command === 'build') {
+        return {
+            ...config
+        }
+    } else {
+        // command === 'dev'
+        return {
+            ...config,
+            define: {
+                global: "window"
+            }
+        }
+
+    }
+})
