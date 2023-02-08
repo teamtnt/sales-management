a8f6763758598ced578356e90f4ab7a987fbd7c2
jbeslic <josipa.beslic@gmail.com>
2023-02-08 06:41:42 +0100

    messages migration

---
+++ database/migrations/sales_management_create_messages_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+
+
+        Schema::create(config('sales-management.tablePrefix').'messages', function (Blueprint $table) {
+            $table->id();
+            $table->string("from_email");
+            $table->string("from_name")->nullable();
+            $table->string("subject")->nullable();
+            $table->text("body")->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop(config('sales-management.tablePrefix').'messages');
+    }
+};
