25c5aaf5f4834f08ad8ae65be9d410b122b62cfe
Nenad Ticaric <nticaric@gmail.com>
2023-02-15 12:47:24 +0100

    workflow

--- src/resources/js/components/Workflow/WorkFlow.vue
+++ src/resources/js/components/Workflow/WorkFlow.vue
@@ -1,8 +1,16 @@
 <script setup>
-import { Panel, VueFlow, useVueFlow } from '@vue-flow/core'
-import { Background } from '@vue-flow/background'
-import { nextTick, watch } from 'vue'
+import {Panel, VueFlow, useVueFlow} from '@vue-flow/core'
+import {Background} from '@vue-flow/background'
+import {nextTick, watch, ref, markRaw} from 'vue'
 import Sidebar from './Sidebar.vue'
+
+import ConditionNode from './ConditionNode.vue'
+import ActionNode from "./ActionNode.vue";
+
+const nodeTypes = {
+    condition: markRaw(ConditionNode),
+    action: markRaw(ActionNode)
+}
 
 const props = defineProps({
     panelTitle: {
@@ -11,10 +19,31 @@ const props = defineProps({
     }
 })
 
+const elements = ref([
+    // Nodes
+    // An input node, specified by using `type: 'input'`
+    {id: '1', type: 'condition', label: 'Node 1', class: 'condition-node', position: {x: 350, y: 100}},
+
+    // Default nodes, you can omit `type: 'default'`
+    {id: '2', type: 'action', label: 'Node 2', position: {x: 100, y: 200},},
+    {id: '3', type: 'action', label: 'Node 3', position: {x: 400, y: 200}},
+
+    // An output node, specified by using `type: 'output'`
+    {id: '4', type: 'condition', label: 'Node 4', position: {x: 400, y: 300}},
+
+    // Edges
+    // Most basic edge, only consists of an id, source-id and target-id
+    {id: 'e1-3', source: '1', target: '3'},
+    {id: 'e1-3', source: '3', target: '4'},
+
+    // An animated edge
+    {id: 'e1-2', source: '1', target: '2', animated: true},
+])
+
 let id = 0
 const getId = () => `dndnode_${id++}`
 
-const { findNode, onConnect, nodes, edges, addEdges, addNodes, viewport, project, vueFlowRef } = useVueFlow({
+const {findNode, onConnect, nodes, edges, addEdges, addNodes, viewport, project, vueFlowRef} = useVueFlow({
     nodes: [
         // {
         //     id: '1',
@@ -38,7 +67,7 @@ onConnect((params) => addEdges([params]))
 const onDrop = (event) => {
     const type = event.dataTransfer?.getData('application/vueflow')
 
-    const { left, top } = vueFlowRef.value.getBoundingClientRect()
+    const {left, top} = vueFlowRef.value.getBoundingClientRect()
 
     const position = project({
         x: event.clientX - left,
@@ -61,11 +90,14 @@ const onDrop = (event) => {
             () => node.dimensions,
             (dimensions) => {
                 if (dimensions.width > 0 && dimensions.height > 0) {
-                    node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+                    node.position = {
+                        x: node.position.x - node.dimensions.width / 2,
+                        y: node.position.y - node.dimensions.height / 2
+                    }
                     stop()
                 }
             },
-            { deep: true, flush: 'post' },
+            {deep: true, flush: 'post'},
         )
     })
 }
@@ -74,13 +106,13 @@ const onDrop = (event) => {
 <template>
     <div class="row w-100 h-100 mx-0" @drop="onDrop">
         <div class="col-md-10 pe-0">
-            <VueFlow @dragover="onDragOver">
-                <Background />
+            <VueFlow @dragover="onDragOver" v-model="elements" :node-types="nodeTypes">
+                <Background/>
                 <Panel><h1 class="h3 mb-3 px-4 py-2 bg-white">{{ panelTitle }}</h1></Panel>
             </VueFlow>
         </div>
-      <div class="col-md-2 px-0">
-        <Sidebar />
-      </div>
+        <div class="col-md-2 px-0">
+            <Sidebar/>
+        </div>
     </div>
 </template>
