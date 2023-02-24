f63c668d59827a6f87e64e5a8eb9df49ca146f13
Tome Perica <tome.perica@yahoo.com>
2023-02-24 16:36:48 +0100

    Update create.blade.php

--- src/resources/views/workflows/create.blade.php
+++ src/resources/views/workflows/create.blade.php
@@ -1,14 +1,8 @@
 @extends('sales-management::layouts.app')
 
 @section('title', 'Workflows')
-@section('breadcrumb')
-    <li class="breadcrumb-item">{{ __('Dashboard') }}</li>
-    <li class="breadcrumb-item">{{ __('Tasks') }}</li>
-    <li class="breadcrumb-item"><a href="{{ route('tasklist.show', $task->id) }}">{{ __($task->name) }}</a></li>
-    <li class="breadcrumb-item"><a href="{{ route('workflows.index', $task->id) }}">{{ __('Workflows') }}</a></li>
-    <li class="breadcrumb-item active">{{ __('Create Workflow') }}</li>
-@endsection
-@section('add-css-class', 'overflow-hidden')
+
+@section('add-css-class', 'overflow-hidden px-0 py-0')
 
 @section('content')
     <work-flow
