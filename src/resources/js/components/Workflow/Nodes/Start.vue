f26c86fa4ee2d25b03bed4f931bcd7bcb2642d4d
jbeslic <josipa.beslic@gmail.com>
2023-03-08 13:18:45 +0100

    rename start to if published

--- src/resources/js/components/Workflow/Nodes/Start.vue
+++ src/resources/js/components/Workflow/Nodes/Start.vue
@@ -23,9 +23,9 @@
     <div class="vue-flow__node-input shadow-sm">
         <span class="action-box pe-2 justify-content-center">
             <span class="condition-box__icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+                <svg style="transform: rotate(315deg);" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-help-circle"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
             </span>
-            Start
+            If published
         </span>
 
         <Handle id="start" type="source" :position="Position.Bottom" :style="sourceHandleStyleTarget" class="handle">
