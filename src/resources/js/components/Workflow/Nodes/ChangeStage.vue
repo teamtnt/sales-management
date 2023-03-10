33aadaece1e7dc9b33f74e4734ad51745184d225
jbeslic <josipa.beslic@gmail.com>
2023-03-10 13:59:13 +0100

    fix and build

--- src/resources/js/components/Workflow/Nodes/ChangeStage.vue
+++ src/resources/js/components/Workflow/Nodes/ChangeStage.vue
@@ -55,7 +55,7 @@
                      class="feather feather-refresh-cw align-middle"><polyline
                     points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline><path
                     d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path></svg></span>
-            Stage Changed</span>
+            Change Stage</span>
 
         <Handle :id="`state.change.stage.target.${id}`" type="target" :position="Position.Top" :style="sourceHandleStyleTargetTop" class="handle">
             <span class="circle"/>
--- src/resources/js/components/Workflow/Sidebar.vue
+++ src/resources/js/components/Workflow/Sidebar.vue
@@ -165,7 +165,7 @@
                     </span>
                 </div>
                 <div class="vue-flow__node-input shadow-sm" :draggable="true"
-                     @dragstart="onDragStart($event, 'action.stage.changed')">
+                     @dragstart="onDragStart($event, 'action.change.stage')">
                     <span class="action-box pe-2 justify-content-end">
                         <span class="action-box__icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
@@ -174,7 +174,7 @@
                                  class="feather feather-refresh-cw align-middle"><polyline
                                 points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline><path
                                 d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path></svg></span>
-                        Stage Changed</span>
+                        Change Stage</span>
                 </div>
             </div>
         </div>
