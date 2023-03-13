7519847f0888687c0e300ea689715d312021782d
Tome Perica <tome.perica@yahoo.com>
2023-03-13 12:22:11 +0100

    add off-canvas trigger on lead card

--- src/resources/views/campaign/show.blade.php
+++ src/resources/views/campaign/show.blade.php
@@ -104,8 +104,16 @@
                 </div>
             @endforeach
         </div>
-
     </div>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    ...
+  </div>
+</div>
 @stop
 
 @push('scripts')
--- src/resources/views/components/lead-card.blade.php
+++ src/resources/views/components/lead-card.blade.php
@@ -1,4 +1,4 @@
-<div class="card mb-3 p-2 bg-light cursor-grab border gap-1 lead-item" data-lead-id="{{ $lead->id }}">
+<div class="lead-item card mb-3 p-2 bg-light border gap-1" data-lead-id="{{ $lead->id }}">
     <div class="d-flex align-items-center">
         <x-sales-management::icons.user/>
         <span class="ms-2">{{ $lead->contact->firstname }} {{ $lead->contact->lastname }}</span>
@@ -11,4 +11,7 @@
         <x-sales-management::icons.phone />
         <span class="ms-2">  {{ $lead->contact->phone }}</span>
     </div>
+    <span class="info-icon" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+        <x-sales-management::icons.info />
+    </span>
 </div>
