2ab85609bcc1d2ba1acb61d8afc0e389b4a43b9d
Tome Perica <tome.perica@yahoo.com>
2023-03-15 10:12:09 +0100

    add missing breadcrumb

--- src/resources/views/campaign/index.blade.php
+++ src/resources/views/campaign/index.blade.php
@@ -1,14 +1,17 @@
 @extends('sales-management::layouts.app')
 
+@section('title', 'Campaigns')
+@section('breadcrumb')
+    <li class="breadcrumb-item">{{ __('Dashboard') }}</li>
+    <li class="breadcrumb-item active">{{ __('Campaigns') }}</li>
+@endsection
+
 @section('content')
-
     <div class="container-fluid p-0">
-
-        <a href="{{ route('campaign.create') }}" class="btn btn-primary float-end mt-n1"><i class="fas fa-plus"></i>
-            New
-            campaign</a>
+        <a href="{{ route('campaign.create') }}" class="btn btn-primary float-end mt-n1">
+            <i class="fas fa-plus me-1"></i>New campaign
+        </a>
         <h1 class="h3 mb-3">Campaigns</h1>
-
         <div class="row">
             <div class="col-12">
                 <div class="card">
