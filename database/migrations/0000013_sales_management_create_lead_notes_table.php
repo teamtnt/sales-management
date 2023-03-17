ade3cf64ad7cb7c9015f95f086f8eda39d01326b
Tome Perica <tome.perica@yahoo.com>
2023-03-17 14:58:43 +0100

    Update 0000013_sales_management_create_lead_notes_table.php

--- database/migrations/0000013_sales_management_create_lead_notes_table.php
+++ database/migrations/0000013_sales_management_create_lead_notes_table.php
@@ -16,18 +16,13 @@ return new class extends Migration {
         Schema::create(config('sales-management.tablePrefix').'lead_notes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger("lead_id")->index();
-            $table->unsignedBigInteger('created_by')->index();
+            $table->integer('created_by')->nullable();
             $table->text("note")->nullable();
             $table->timestamps();
 
             $table->foreign('lead_id')
                 ->references('id')
                 ->on(config('sales-management.tablePrefix').'leads')
-                ->cascadeOnDelete();
-
-            $table->foreign('created_by')
-                ->references('id')
-                ->on('users')
                 ->cascadeOnDelete();
         });
     }
